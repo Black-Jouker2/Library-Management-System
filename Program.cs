c807fa29021de3ad482739fc013e59f0c74d4978
AHMed <AHMed@DESKTOP-LNL9S34>
2026-07-05 13:04:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace LibraryManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.ReadLine();
+                Console.WriteLine("Hello, World!");
+            }
+
+        }
+    }
+}

77ed944dabed0c1ec4fe3c477048944a58532651
Black-Jouker2 <aaahmed.salahhhhh0186@gmail.com>
2026-07-12 18:29:23 +0300

    Update Member.cs

--- Models/Member.cs
+++ Models/Member.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library_Management_System.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace Library_Management_System.Models
 {
-    internal class Member
+
+
+    public abstract class Member : ISearchable
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime JoinDate { get; set; }
+
+        protected Member(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            JoinDate = DateTime.Now; 
+        }
+
+        public abstract string GetMemberType();
+
+        // تنفيذ واجهة البحث
+        public bool MatchesQuery(string query)
+        {
+            string q = query.ToLowerInvariant();
+            return Name.ToLowerInvariant().Contains(q) ||
+                   Email.ToLowerInvariant().Contains(q);
+        }
+
+        public override string ToString()
+        {
+            return $"{GetMemberType()} Member #{Id}: {Name} | {Email} | Joined: {JoinDate:yyyy-MM-dd HH:mm}";
+        }
     }
+
+
 }

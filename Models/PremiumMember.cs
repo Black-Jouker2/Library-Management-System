3e59418e585a52f6774f7a46220e6161f0e0fbb6
Black-Jouker2 <aaahmed.salahhhhh0186@gmail.com>
2026-07-12 18:28:55 +0300

    Update PremiumMember.cs
    Models-pro-regular

--- Models/PremiumMember.cs
+++ Models/PremiumMember.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Library_Management_System.Models
 {
-    internal class PremiumMember
+    public class PremiumMember : Member
     {
+        public int MaxBooksAllowed { get; set; } = 10;
+
+        public PremiumMember(int id, string name, string email) : base(id, name, email) { }
+
+        public override string GetMemberType() => "Premium";
+
+        public override string ToString()
+        {
+            return base.ToString() + $" | Max Books: {MaxBooksAllowed}";
+        }
+    }
+
+    public class RegularMember : Member
+    {
+        public RegularMember(int id, string name, string email) : base(id, name, email) { }
+
+        public override string GetMemberType() => "Regular";
     }
 }

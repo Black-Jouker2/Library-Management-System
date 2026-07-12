0c87535beb504d7691b5ba0210970e334359cea0
Black-Jouker2 <aaahmed.salahhhhh0186@gmail.com>
2026-07-12 18:29:15 +0300

    Update Book.cs

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library_Management_System.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace Library_Management_System.Models
 {
-    internal class Book
+    public class Book : LibraryItem, ISearchable
     {
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public bool IsAvailable { get; set; } = true;
+
+        public Book(int id, string title, string author, int year, string genre)
+            : base(id, title)
+        {
+            Author = author;
+            Year = year;
+            Genre = genre;
+        }
+
+        // تنفيذ واجهة البحث
+        public bool MatchesQuery(string query)
+        {
+            string q = query.ToLowerInvariant();
+            return Title.ToLowerInvariant().Contains(q) ||
+                   Author.ToLowerInvariant().Contains(q) ||
+                   Genre.ToLowerInvariant().Contains(q) ||
+                   Year.ToString().Contains(q);
+        }
+
+        public override string GetInfo()
+        {
+            return $"ID: {Id} | {Title} by {Author} ({Year}) [{Genre}] – {(IsAvailable ? "Available" : "Borrowed")}";
+        }
+
+        public override string ToString() => GetInfo();
     }
 }
